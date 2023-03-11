0bcee6dbb1cd5f222915e0ee5578ddda457cc281
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-11 14:01:46 +0530

    config.ts created and its using dotenv for get database info.

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const port = process.env.PORT || 3010;
+export const mysqlConfig = {
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_DB_USER,
+  password: process.env.SQL_DB_PASSWORD,
+  database: process.env.SQL_DB,
+};
+export const neo4jConfig = {
+  url: process.env.NEO4J_DB_URL || "",
+  user: process.env.NEO4J_DB_USER || "",
+  password: process.env.NEO4J_DB_PASSWORD || "",
+};
