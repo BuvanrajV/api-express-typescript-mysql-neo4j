ac80ec3f05178ffc5462575d5dc76465b935a725
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-12 19:54:40 +0530

    controller.ts updated. Getting body data and that datas are inserted into database

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from "express";
-import { createUser, getUserId } from "../models/mysqlModel";
-import { createUserNode } from "../models/neo4jModel";
+import { insertDetailsInUserdepartmentMapping } from "./../models/mysqlModels/userDepartmentMapping";
+import { createUser, getUserId } from "../models/mysqlModels/usersModel";
+import { getLocationId } from "../models/mysqlModels/locationsModel";
+import {
+  createUserNode,
+  userDepartmentRelationship,
+  userLocationRelationship,
+} from "../models/neo4jModel";
+import { insertDetailsInUserlocationMapping } from "../models/mysqlModels/userLocationMappingModel";
+import { getDepartmentId } from "../models/mysqlModels/departmentsModel";
 
 export const postController = async (req: Request, res: Response) => {
   try {
@@ -10,18 +18,41 @@ export const postController = async (req: Request, res: Response) => {
       user_email: req.body.email,
       user_startdate: req.body.startDate,
       user_designation: req.body.designation,
-        user_created_date: new Date(),
+      user_created_date: new Date(),
       user_status: "1",
     };
     await createUser(userDetailsForMysql);
-    const getId: any = await getUserId(userDetailsForMysql.user_email);
-    const id: any = getId[0].user_id;
+    let getUserIdFromDb: any = await getUserId(userDetailsForMysql.user_email);
+    const userId = getUserIdFromDb[0].user_id;
     const userDetailsForNeo4j = {
-      user_id: id,
+      user_id: userId,
       ...userDetailsForMysql,
     };
     await createUserNode(userDetailsForNeo4j);
-    console.log("successfully created");
+    const getLocationIdFromDb: any = await getLocationId(req.body.location);
+    const locationId = getLocationIdFromDb[0].location_id;
+    const userDetailsForUserLocationMapping = {
+      map_user_id: userId,
+      map_location_id: locationId,
+      map_created_date: new Date(),
+      map_status: "1",
+    };
+    await insertDetailsInUserlocationMapping(userDetailsForUserLocationMapping);
+    await userLocationRelationship(userId, locationId);
+    const getDepartmentIdFromDb: any = await getDepartmentId(
+      req.body.department
+    );
+    const departmentId = getDepartmentIdFromDb[0].id;
+    const userDetailsForUserDepartmentMapping = {
+      map_user_id: userId,
+      map_department_id: departmentId,
+      map_created_date: new Date(),
+      map_status: "1",
+    };
+    await insertDetailsInUserdepartmentMapping(
+      userDetailsForUserDepartmentMapping
+    );
+    await userDepartmentRelationship(userId, departmentId);
   } catch {
     console.log("error occur");
   }
