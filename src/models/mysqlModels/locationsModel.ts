530136cedea1710bde571c1e301519bc4b58b928
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-13 15:28:31 +0530

    code readability increased by using prettier in locationsModel.ts

--- src/models/mysqlModels/locationsModel.ts
+++ src/models/mysqlModels/locationsModel.ts
@@ -1,14 +1,18 @@
 import mysqlDb from "../../databases/mysqlDatabase";
 
-export const getLocationId = (location:string)=>{
-    return new Promise ((resolve,reject)=>{
-      mysqlDb.query ("SELECT location_id from locations where location_name=?",[location],(err,res)=>{
-        if(err){
-          console.error("error : ",err)
-          reject(err)
-        }else{
-          resolve(res)
+export const getLocationId = (location: string) => {
+  return new Promise((resolve, reject) => {
+    mysqlDb.query(
+      "SELECT location_id from locations where location_name=?",
+      [location],
+      (err, res) => {
+        if (err) {
+          console.error("error : ", err);
+          reject(err);
+        } else {
+          resolve(res);
         }
-      })
-    })
-  }
+      }
+    );
+  });
+};
