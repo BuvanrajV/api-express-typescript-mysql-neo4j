0eff62cac2c532ba87d6c624696db6d4a9c53d9a
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-13 15:29:02 +0530

    code readability increased by using prettier in userLocationMappingModel.ts

--- src/models/mysqlModels/userLocationMappingModel.ts
+++ src/models/mysqlModels/userLocationMappingModel.ts
@@ -7,15 +7,21 @@ interface UserDetails {
   map_status: string;
 }
 
-export const insertDetailsInUserlocationMapping = (userDetails: UserDetails) => {
+export const insertDetailsInUserlocationMapping = (
+  userDetails: UserDetails
+) => {
   return new Promise((resolve, reject) => {
-    mysqlDb.query("INSERT INTO user_location_mapping SET ?",[userDetails],(err,res)=>{
-        if(err){
-            console.error(err)
-            reject()
-        }else{
-            resolve(res)
+    mysqlDb.query(
+      "INSERT INTO user_location_mapping SET ?",
+      [userDetails],
+      (err, res) => {
+        if (err) {
+          console.error(err);
+          reject();
+        } else {
+          resolve(res);
         }
-    });
+      }
+    );
   });
 };
