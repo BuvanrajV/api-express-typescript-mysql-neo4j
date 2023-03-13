998c84ce833762dd0c20f6e4a3383e0097fcdbd9
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-13 17:39:10 +0530

    In userModel.ts getCandidateById created

--- src/models/mysqlModels/usersModel.ts
+++ src/models/mysqlModels/usersModel.ts
@@ -8,13 +8,30 @@ interface UserDetails {
   user_designation: string;
   user_created_date: Date;
   user_status: string;
-} 
+}
 
 export const getUserId = (email: { user_email: string }) => {
   return new Promise((resolve, reject) => {
     mysqlDb.query(
       "Select user_id from users where user_email=?",
       [email],
+      (err, res) => {
+        if (err) {
+          console.error("error : ", err);
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      }
+    );
+  });
+};
+
+export const getCandidateById = (candidateId: number) => {
+  return new Promise((resolve, reject) => {
+    mysqlDb.query(
+      "Select * from users where user_candidate_id=?",
+      [candidateId],
       (err, res) => {
         if (err) {
           console.error("error : ", err);
@@ -39,3 +56,4 @@ export const createUser = (userDetails: UserDetails) => {
     });
   });
 };
+
