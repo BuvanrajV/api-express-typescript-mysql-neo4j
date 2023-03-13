8bfd11a849b4db32546b0780e072f12f7c3d2389
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-13 15:28:10 +0530

    code readability increased by using prettier in departmentsModel.ts

--- src/models/mysqlModels/departmentsModel.ts
+++ src/models/mysqlModels/departmentsModel.ts
@@ -1,14 +1,18 @@
 import mysqlDb from "../../databases/mysqlDatabase";
 
-export const getDepartmentId = (department:string)=>{
-    return new Promise ((resolve,reject)=>{
-      mysqlDb.query ("SELECT id from departments where department_name=?",[department],(err,res)=>{
-        if(err){
-          console.error("error : ",err)
-          reject(err)
-        }else{
-          resolve(res)
+export const getDepartmentId = (department: string) => {
+  return new Promise((resolve, reject) => {
+    mysqlDb.query(
+      "SELECT id from departments where department_name=?",
+      [department],
+      (err, res) => {
+        if (err) {
+          console.error("error : ", err);
+          reject(err);
+        } else {
+          resolve(res);
         }
-      })
-    })
-  }
+      }
+    );
+  });
+};
