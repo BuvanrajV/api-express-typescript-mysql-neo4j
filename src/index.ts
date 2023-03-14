87400dd242d343edc07b3fb6aba9208d82d13458
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-14 12:40:42 +0530

    double quotes are chnaged into single quote in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import routes from "./routes/routes";
-import { port } from "./config/config";
+import express from 'express'
+import routes from './routes/routes'
+import { port } from './config/config'
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.use("/", routes);
+app.use('/', routes)
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+  console.log(`Server is listening on port ${port}`)
+})
