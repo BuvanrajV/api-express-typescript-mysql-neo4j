eff601ed8a059e35f1b3df54cd554e5e122173ff
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-14 12:47:29 +0530

    initial validation updated in routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,47 +1,68 @@
-import { Router } from "express";
-import { body } from "express-validator";
+import { Router } from 'express'
+import { body } from 'express-validator'
 import {
   postController,
   putController,
   deleteController,
-} from "../controllers/controller";
-import { postMiddleware, deleteMiddleware } from "../middlewares/middleware";
+} from '../controllers/controller'
+import { postMiddleware, deleteMiddleware } from '../middlewares/middleware'
 
-const router = Router();
+const router = Router()
 
 router.post(
   // Endpoint
-  "/post",
+  '/post',
   // Initial data validation using express validator
-  body("firstName").isLength({ max: 150 }).isAlphanumeric(),
-  body("lastName").isLength({ max: 150 }).isAlphanumeric(),
-  body("email").isEmail().withMessage("email should be in email format"),
-  body("startDate")
-    .isDate()
-    .withMessage("Date should be in YYYY-MM-DD this format")
-    .custom((startDate) => {
-      // Check the startDate is greater than or equal to current date
-      let currentDateAndTime = new Date();
-      let currentDate = currentDateAndTime.toISOString().slice(0, 10);
-      if (startDate < currentDate) {
-        throw new Error(
-          "startDate should be greater than or equal to current date"
-        );
-      } else {
-        return "no Error";
-      }
-    }),
-  body("location").isLength({ max: 100 }),
-  body("department").isLength({ max: 100 }),
-  body("designation").isLength({ max: 100 }),
-  body("candidateId").isLength({ max: 10 }),
+  [
+    body('candidateId')
+      .isLength({ max: 10 })
+      .notEmpty()
+      .withMessage('CandidateId Should not be empty'),
+    body('firstName')
+      .isLength({ max: 150 })
+      .isAlphanumeric()
+      .withMessage('Firstname should be in alphanumeric'),
+    body('lastName')
+      .isLength({ max: 150 })
+      .isAlphanumeric()
+      .withMessage('Lastname Should be in alphanumeric'),
+    body('email').isEmail().withMessage('email should be in email format'),
+    body('startDate')
+      .isDate()
+      .withMessage('Date should be in YYYY-MM-DD this format')
+      .custom((startDate) => {
+        // Check the startDate is greater than or equal to current date
+        let currentDateAndTime = new Date()
+        let currentDate = currentDateAndTime.toISOString().slice(0, 10)
+        if (startDate < currentDate) {
+          throw new Error(
+            'startDate should be greater than or equal to current date'
+          )
+        } else {
+          return 'no Error'
+        }
+      }),
+    body('location')
+      .isLength({ max: 100 })
+      .exists()
+      .withMessage('location field is missing')
+      .notEmpty()
+      .withMessage('Location should not be empty'),
+    body('department')
+      .isLength({ max: 100 })
+      .exists()
+      .withMessage('department field is missing')
+      .notEmpty()
+      .withMessage('Department should not be empty'),
+    body('designation').isLength({ max: 100 }),
+  ],
   // Middleware
   postMiddleware,
   // Controller
   postController
-);
+)
 
-router.put("/", putController);
-router.delete("/delete", deleteMiddleware, deleteController);
+router.put('/', putController)
+router.delete('/delete', deleteMiddleware, deleteController)
 
-export default router;
+export default router
