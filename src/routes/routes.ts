f08c7f9593bcf2eaf55a8d9d712e49deb137c604
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-15 15:54:54 +0530

    router.put added in routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,11 @@ import {
   putController,
   deleteController,
 } from '../controllers/controller'
-import { postMiddleware, deleteMiddleware } from '../middlewares/middlewares'
+import {
+  postMiddleware,
+  putMiddleware,
+  deleteMiddleware,
+} from '../middlewares/middlewares'
 
 const router = Router()
 
@@ -13,6 +17,68 @@ router.post(
   // Endpoint
   '/post',
   // Initial data validation using express validator
+  [
+    body('candidateId')
+      .isNumeric()
+      .exists()
+      .isLength({ max: 10 })
+      .notEmpty()
+      .withMessage('CandidateId Should not be empty'),
+    body('firstName')
+      .isString()
+      .exists()
+      .isLength({ max: 150 })
+      .isAlphanumeric()
+      .withMessage('Firstname should be in alphanumeric'),
+    body('lastName')
+      .isString()
+      .isLength({ max: 150 })
+      .isAlphanumeric()
+      .withMessage('Lastname Should be in alphanumeric'),
+    body('email')
+      .isString()
+      .exists()
+      .isEmail()
+      .withMessage('email should be in email format'),
+    body('startDate')
+      .isString()
+      .exists()
+      .isDate()
+      .withMessage('Date should be in YYYY-MM-DD this format')
+      .custom((startDate) => {
+        // Check the startDate is greater than or equal to current date
+        let currentDateAndTime = new Date()
+        let currentDate = currentDateAndTime.toISOString().slice(0, 10)
+        if (startDate < currentDate) {
+          throw new Error(
+            'startDate should be greater than or equal to current date'
+          )
+        } else {
+          return 'no Error'
+        }
+      }),
+    body('location')
+      .isLength({ max: 100 })
+      .exists()
+      .withMessage('location field is missing')
+      .notEmpty()
+      .withMessage('Location should not be empty'),
+    body('department')
+      .isLength({ max: 100 })
+      .exists()
+      .withMessage('department field is missing')
+      .notEmpty()
+      .withMessage('Department should not be empty'),
+    body('designation').isLength({ max: 100 }),
+  ],
+  // Middleware
+  postMiddleware,
+  // Controller
+  postController
+)
+
+router.put(
+  '/put', // Initial data validation using express validator
   [
     body('candidateId')
       .isNumeric()
@@ -51,25 +117,19 @@ router.post(
       }),
     body('location')
       .isLength({ max: 100 })
-      .exists()
       .withMessage('location field is missing')
       .notEmpty()
       .withMessage('Location should not be empty'),
     body('department')
       .isLength({ max: 100 })
-      .exists()
       .withMessage('department field is missing')
       .notEmpty()
       .withMessage('Department should not be empty'),
     body('designation').isLength({ max: 100 }),
   ],
-  // Middleware
-  postMiddleware,
-  // Controller
-  postController
+  putMiddleware,
+  putController
 )
-
-router.put('/', putController)
 router.delete('/delete', deleteMiddleware, deleteController)
 
 export default router
