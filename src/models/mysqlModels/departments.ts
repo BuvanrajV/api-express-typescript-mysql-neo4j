145ade350acbee9a9313bb11af43c0d27c6a2a81
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-15 15:51:43 +0530

    departments.ts updated. getDepartmentName added

--- src/models/mysqlModels/departments.ts
+++ src/models/mysqlModels/departments.ts
@@ -1,18 +1,29 @@
 import mysqlDb from '../../databases/mysql'
 
-export const getDepartmentId = (department: string) => {
+
+
+
+const runQuery = (query: string, input: any) => {
   return new Promise((resolve, reject) => {
-    mysqlDb.query(
-      'SELECT id from departments where department_name=?',
-      [department],
-      (err, res) => {
-        if (err) {
-          console.error('error : ', err)
-          reject(err)
-        } else {
-          resolve(res)
-        }
+    mysqlDb.query(query, input, (err, res) => {
+      if (err) {
+        console.error('error : ', err)
+        reject(err)
+      } else {
+        resolve(res)
       }
-    )
+    })
   })
+}
+
+
+export const getDepartmentId = (department: string) => {
+  const query = 'SELECT id from departments where department_name=?'
+  return runQuery(query,department)
+}
+
+
+export const getDepartmentName = (departmentId : number)=>{
+  const query='SELECT department_name from departments where id=?'
+  return runQuery(query,departmentId)
 }
