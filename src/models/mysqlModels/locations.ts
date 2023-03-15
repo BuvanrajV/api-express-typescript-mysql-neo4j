39eaea0aeb0c206d00e4b8eb8a2ea3dc4b2890ae
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-15 15:52:19 +0530

    locations.ts updated. getLocationName added

--- src/models/mysqlModels/locations.ts
+++ src/models/mysqlModels/locations.ts
@@ -1,18 +1,25 @@
 import mysqlDb from '../../databases/mysql'
 
-export const getLocationId = (location: string) => {
+
+const runQuery = (query: string, input: any) => {
   return new Promise((resolve, reject) => {
-    mysqlDb.query(
-      'SELECT location_id from locations where location_name=?',
-      [location],
-      (err, res) => {
-        if (err) {
-          console.error('error : ', err)
-          reject(err)
-        } else {
-          resolve(res)
-        }
+    mysqlDb.query(query, input, (err, res) => {
+      if (err) {
+        console.error('error : ', err)
+        reject(err)
+      } else {
+        resolve(res)
       }
-    )
+    })
   })
+}
+
+export const getLocationId = (location: string) => {
+  const query='SELECT location_id from locations where location_name=?'
+  return runQuery(query,location)
+}
+
+export const getLocationName = (locationId : number)=>{
+  const query='SELECT location_name from locations where location_id=?'
+  return runQuery(query,locationId)
 }
