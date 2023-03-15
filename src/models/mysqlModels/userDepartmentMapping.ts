91a4ac2143626489d25b74d545986d81105dcec4
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-15 15:53:16 +0530

    Insertion and deletion query function added in userDepartmentMapping.ts

--- src/models/mysqlModels/userDepartmentMapping.ts
+++ src/models/mysqlModels/userDepartmentMapping.ts
@@ -7,21 +7,34 @@ interface UserDetails {
   map_status: string;
 }
 
+
+const runQuery = (query: string, input: any) => {
+  return new Promise((resolve, reject) => {
+    mysqlDb.query(query, input, (err, res) => {
+      if (err) {
+        console.error('error : ', err)
+        reject(err)
+      } else {
+        resolve(res)
+      }
+    })
+  })
+}
+
 export const insertInUserdepartmentMapping = (
   userDetails: UserDetails
 ) => {
-  return new Promise((resolve, reject) => {
-    mysqlDb.query(
-      'INSERT INTO user_department_mapping SET ?',
-      [userDetails],
-      (err, res) => {
-        if (err) {
-          console.error(err)
-          reject()
-        } else {
-          resolve(res)
-        }
-      }
-    )
-  })
+  const query='INSERT INTO user_department_mapping SET ?'
+  return runQuery(query,userDetails)
+}
+
+
+export const getDepartmentIdFromMapping = (userId: number) => {
+  const query='SELECT map_department_id from user_department_mapping WHERE map_user_id=?'
+  return runQuery(query,userId)
+}
+
+export const changeMapStatusInDepartment = (userId:number)=>{
+  const query = 'UPDATE user_department_mapping SET map_status="0" WHERE map_user_id=?'
+  return runQuery(query,userId)
 }
