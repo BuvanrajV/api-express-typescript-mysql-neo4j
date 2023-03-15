a117f7ae75746f8e460e9c4d64b89162dedcf3f6
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-15 15:49:53 +0530

    putMiddleware added in middlewares.ts

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,3 +1,4 @@
+import { checkEmail } from './../models/mysqlModels/users'
 import { NextFunction, Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 import { getLocationId } from '../models/mysqlModels/locations'
@@ -9,6 +10,7 @@ export const postMiddleware = async (
   res: Response,
   next: NextFunction
 ) => {
+  req.body.candidateId = parseInt(req.body.candidateId)
   // Check any error occurred in initial data validation
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
@@ -37,7 +39,7 @@ export const postMiddleware = async (
   }
   // User details created
   req.body.userDetailsForMysql = {
-    user_candidate_id: parseInt(req.body.candidateId),
+    user_candidate_id: req.body.candidateId,
     user_first_name: req.body.firstName,
     user_last_name: req.body.lastName,
     user_email: req.body.email,
@@ -49,12 +51,51 @@ export const postMiddleware = async (
   next()
 }
 
+export const putMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  req.body.candidateId = parseInt(req.body.candidateId)
+  const candidate: any = await getCandidateById(req.body.candidateId)
+  if (!candidate[0]) {
+    return res.status(400).json({ error: 'Enter valid candidate Id' })
+  }
+  req.body.userId=candidate[0].user_id
+  const emailExist: any = await checkEmail(
+    req.body.email,
+    req.body.candidateId
+  )
+  if (emailExist[0]) {
+    return res.status(400).json({ error: 'Email already Exist' })
+  }
+  // Check Location exist or not
+  const locationId: any = await getLocationId(req.body.location)
+  if (!locationId[0]) {
+    return res.status(400).json({ errror: 'Location not found. Invalid!' })
+  }
+  // Check Department exist or not
+  const departmentId: any = await getDepartmentId(req.body.department)
+  if (!departmentId[0]) {
+    return res.status(400).json({ error: 'Department not found. Invalid!' })
+  }
+  req.body.userDetails = {
+    user_first_name: req.body.firstName,
+    user_last_name: req.body.lastName,
+    user_email: req.body.email,
+    user_startdate: req.body.startDate,
+    user_designation: req.body.designation,
+    user_updated_date: new Date(),
+  }
+  next()
+}
+
 export const deleteMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  req.body.candidateId=parseInt(req.body.candidateId)
+  req.body.candidateId = parseInt(req.body.candidateId)
   const candidate: any = await getCandidateById(req.body.candidateId)
   if (!candidate[0]) {
     return res.status(400).json({ error: 'Enter valid candidate Id' })
