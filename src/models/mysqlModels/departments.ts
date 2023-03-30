8a3e07dd14c16588e9e0e8eda36b3c1ebe179b65
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 12:00:44 +0530

    runQuery function removed in departments.ts

--- src/models/mysqlModels/departments.ts
+++ src/models/mysqlModels/departments.ts
@@ -1,29 +1,11 @@
-import mysqlDb from '../../databases/mysql'
-
-
-
-
-const runQuery = (query: string, input: any) => {
-  return new Promise((resolve, reject) => {
-    mysqlDb.query(query, input, (err, res) => {
-      if (err) {
-        console.error('error : ', err)
-        reject(err)
-      } else {
-        resolve(res)
-      }
-    })
-  })
-}
-
+import { runQuery } from './../../databases/mysql'
 
 export const getDepartmentId = (department: string) => {
   const query = 'SELECT id FROM departments WHERE department_name=?'
-  return runQuery(query,department)
+  return runQuery(query, department)
 }
 
-
-export const getDepartmentName = (departmentId : number)=>{
-  const query='SELECT department_name FROM departments WHERE id=?'
-  return runQuery(query,departmentId)
+export const getDepartmentName = (departmentId: number) => {
+  const query = 'SELECT department_name FROM departments WHERE id=?'
+  return runQuery(query, departmentId)
 }
