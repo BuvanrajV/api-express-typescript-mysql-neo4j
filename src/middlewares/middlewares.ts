6cccd18a85ac89d5a31098ab7eaf85249f1f301c
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 13:35:43 +0530

    middleware.ts updated

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,41 +1,58 @@
-import { checkEmail } from './../models/mysqlModels/users'
-import { NextFunction, Request, Response } from 'express'
-import { validationResult } from 'express-validator'
-import { getLocationId } from '../models/mysqlModels/locations'
-import { getUserId, getCandidateById } from '../models/mysqlModels/users'
-import { getDepartmentId } from '../models/mysqlModels/departments'
+import { checkEmail } from "./../models/mysqlModels/users";
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { createLocation, getLocationId } from "../models/mysqlModels/locations";
+import { getUserId, getCandidateById } from "../models/mysqlModels/users";
+import {
+  createDepartment,
+  getDepartmentId,
+} from "../models/mysqlModels/departments";
+import {
+  createDepartmentNode,
+  createLocationNode,
+} from "../models/usersInNeo4j";
 
 export const postMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  req.body.candidateId = parseInt(req.body.candidateId)
+  req.body.candidateId = parseInt(req.body.candidateId);
   // Check any error occurred in initial data validation
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ error: errors.array() })
+    return res.status(400).json({ error: errors.array() });
   }
   // Check CandidateId exist or not
-  const candidate: any = await getCandidateById(req.body.candidateId)
+  const candidate: any = await getCandidateById(req.body.candidateId);
   if (candidate[0]) {
-    return res.status(400).json({ error: 'Candidate Id already exist' })
+    return res.status(400).json({ error: "Candidate Id already exist" });
   }
   // Check Email exist or not
-  const userId: any = await getUserId(req.body.email)
+  const userId: any = await getUserId(req.body.email);
 
   if (userId[0]) {
-    return res.status(400).json({ error: 'Email already exist' })
+    return res.status(400).json({ error: "Email already exist" });
   }
   // Check Location exist or not
-  const locationId: any = await getLocationId(req.body.location)
+  const locationId: any = await getLocationId(req.body.location);
+  // Create location in db
   if (!locationId[0]) {
-    return res.status(400).json({ errror: 'Location not found. Invalid!' })
+    await createLocation(req.body.location);
+    const getLocationIdFromDb: any = await getLocationId(req.body.location);
+    const locationId = getLocationIdFromDb[0].location_id;
+    await createLocationNode(locationId, req.body.location);
   }
   // Check Department exist or not
-  const departmentId: any = await getDepartmentId(req.body.department)
+  const departmentId: any = await getDepartmentId(req.body.department);
+  // Create department in db
   if (!departmentId[0]) {
-    return res.status(400).json({ error: 'Department not found. Invalid!' })
+    await createDepartment(req.body.department);
+    const getDepartmentIdFromDb: any = await getDepartmentId(
+      req.body.department
+    );
+    const departmentId = getDepartmentIdFromDb[0].id;
+    await createDepartmentNode(departmentId, req.body.department);
   }
   // User details created
   req.body.userDetailsForMysql = {
@@ -46,38 +63,43 @@ export const postMiddleware = async (
     user_startdate: req.body.startDate,
     user_designation: req.body.designation,
     user_created_date: new Date(),
-    user_status: '1',
-  }
-  next()
-}
+    user_status: "1",
+  };
+  next();
+};
 
 export const putMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  req.body.candidateId = parseInt(req.body.candidateId)
-  const candidate: any = await getCandidateById(req.body.candidateId)
-  if (!candidate[0]) {
-    return res.status(400).json({ error: 'Enter valid candidate Id' })
+  req.body.candidateId = parseInt(req.body.candidateId);
+  const userId: any = await getCandidateById(req.body.candidateId);
+  if (!userId[0]) {
+    return res.status(400).json({ error: "Enter valid candidate Id" });
   }
-  req.body.userId = candidate[0].user_id
-  const emailExist: any = await checkEmail(
-    req.body.email,
-    req.body.candidateId
-  )
+  req.body.userId = userId;
+  const emailExist: any = await checkEmail(req.body.email, userId);
   if (emailExist[0]) {
-    return res.status(400).json({ error: 'Email already Exist' })
+    return res.status(400).json({ error: "Email already Exist" });
   }
   // Check Location exist or not
-  const locationId: any = await getLocationId(req.body.location)
+  const locationId: any = await getLocationId(req.body.location);
   if (!locationId[0]) {
-    return res.status(400).json({ errror: 'Location not found. Invalid!' })
+    await createLocation(req.body.location);
+    const getLocationIdFromDb: any = await getLocationId(req.body.location);
+    const locationId = getLocationIdFromDb[0].location_id;
+    await createLocationNode(locationId, req.body.location);
   }
   // Check Department exist or not
-  const departmentId: any = await getDepartmentId(req.body.department)
+  const departmentId: any = await getDepartmentId(req.body.department);
   if (!departmentId[0]) {
-    return res.status(400).json({ error: 'Department not found. Invalid!' })
+    await createDepartment(req.body.department);
+    const getDepartmentIdFromDb: any = await getDepartmentId(
+      req.body.department
+    );
+    const departmentId = getDepartmentIdFromDb[0].id;
+    await createDepartmentNode(departmentId, req.body.department);
   }
   req.body.userDetails = {
     user_first_name: req.body.firstName,
@@ -86,19 +108,20 @@ export const putMiddleware = async (
     user_startdate: req.body.startDate,
     user_designation: req.body.designation,
     user_updated_date: new Date(),
-  }
-  next()
-}
+  };
+  next();
+};
 
 export const deleteMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  req.body.candidateId = parseInt(req.body.candidateId)
-  const candidate: any = await getCandidateById(req.body.candidateId)
-  if (!candidate[0]) {
-    return res.status(400).json({ error: 'Enter valid candidate Id' })
+  req.body.candidateId = parseInt(req.body.candidateId);
+  const userId: any = await getCandidateById(req.body.candidateId);
+  if (!userId[0]) {
+    return res.status(400).json({ error: "Enter valid candidate Id" });
   }
-  next()
-}
+  req.body.userId = userId;
+  next();
+};
