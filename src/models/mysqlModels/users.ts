8374bdec0a15ca0777c4492be78c8585e4123187
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 10:33:26 +0530

    queries updated  in users.ts

--- src/models/mysqlModels/users.ts
+++ src/models/mysqlModels/users.ts
@@ -25,12 +25,13 @@ const runQuery = (query: string, input: any) => {
 }
 
 export const getUserId = (email: { user_email: string }) => {
-  const query = 'Select user_id from users where user_email=?'
+  const query =
+    'SELECT user_id FROM users WHERE user_email=? AND user_status="1"'
   return runQuery(query, email)
 }
 
 export const getCandidateById = (candidateId: number) => {
-  const query = 'Select * from users where user_candidate_id=?'
+  const query = 'SELECT * FROM users WHERE user_candidate_id=? AND user_status="1"'
   return runQuery(query, candidateId)
 }
 
@@ -40,16 +41,17 @@ export const createUser = (userDetails: UserDetails) => {
 }
 
 export const updateUser = (candidateId: number, userDetails: UserDetails) => {
-  const query = 'UPDATE users SET ? WHERE user_candidate_id=?'
+  const query = 'UPDATE users SET ? WHERE user_candidate_id=? AND user_status="1"'
   return runQuery(query, [userDetails, candidateId])
 }
 
 export const changeUserStatusInMysql = (candidateId: number) => {
-  const query = 'UPDATE users SET user_status=\'0\' WHERE user_candidate_id=?'
+  const query = 'UPDATE users SET user_status="0" WHERE user_candidate_id=?'
   return runQuery(query, candidateId)
 }
 
-export const checkEmail = (email:string,candidateId:number)=>{
-  const query = 'SELECT * from users where user_email=? and not user_candidate_id=?'
-  return runQuery(query,[email,candidateId])
+export const checkEmail = (email: string, candidateId: number) => {
+  const query =
+    'SELECT * FROM users WHERE user_email=? AND user_candidate_id!=?'
+  return runQuery(query, [email, candidateId])
 }
