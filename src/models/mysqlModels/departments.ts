cf503bd3cf27a931580bdc2488dfcb7e2aa553bf
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 13:36:32 +0530

    createDepartment query added in departments.ts

--- src/models/mysqlModels/departments.ts
+++ src/models/mysqlModels/departments.ts
@@ -1,5 +1,11 @@
 import { runQuery } from './../../databases/mysql'
 
+
+export const createDepartment = (department: string) => {
+  const query='INSERT INTO departments (department_name,department_status) VALUES (?,"1")'
+  return runQuery(query,department)
+}
+
 export const getDepartmentId = (department: string) => {
   const query = 'SELECT id FROM departments WHERE department_name=?'
   return runQuery(query, department)
