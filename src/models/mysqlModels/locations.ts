74391675943db10e26a0899354d9ebe164d19c3d
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 12:01:07 +0530

    runQuery function removed in locations.ts

--- src/models/mysqlModels/locations.ts
+++ src/models/mysqlModels/locations.ts
@@ -1,18 +1,5 @@
-import mysqlDb from '../../databases/mysql'
+import { runQuery } from '../../databases/mysql'
 
-
-const runQuery = (query: string, input: any) => {
-  return new Promise((resolve, reject) => {
-    mysqlDb.query(query, input, (err, res) => {
-      if (err) {
-        console.error('error : ', err)
-        reject(err)
-      } else {
-        resolve(res)
-      }
-    })
-  })
-}
 
 export const getLocationId = (location: string) => {
   const query='SELECT location_id FROM locations WHERE location_name=?'
