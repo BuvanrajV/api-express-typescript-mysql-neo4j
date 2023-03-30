421f46be06dccf913dae38371c8cfadfc184a4da
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 12:01:33 +0530

    queries changed in userDepartmentMApping.ts

--- src/models/mysqlModels/userDepartmentMapping.ts
+++ src/models/mysqlModels/userDepartmentMapping.ts
@@ -1,4 +1,4 @@
-import mysqlDb from '../../databases/mysql'
+import { runQuery } from '../../databases/mysql'
 
 interface UserDetails {
   map_user_id: number;
@@ -7,34 +7,28 @@ interface UserDetails {
   map_status: string;
 }
 
-
-const runQuery = (query: string, input: any) => {
-  return new Promise((resolve, reject) => {
-    mysqlDb.query(query, input, (err, res) => {
-      if (err) {
-        console.error('error : ', err)
-        reject(err)
-      } else {
-        resolve(res)
-      }
-    })
-  })
+export const insertInUserdepartmentMapping = (userDetails: UserDetails) => {
+  const { map_user_id, map_department_id, map_created_date, map_status } =
+    userDetails
+  const query =
+    `INSERT INTO user_department_mapping (map_user_id,map_department_id,map_created_date,map_status)
+     VALUES (?,?,?,?)`
+  return runQuery(query, [
+    map_user_id,
+    map_department_id,
+    map_created_date,
+    map_status,
+  ])
 }
-
-export const insertInUserdepartmentMapping = (
-  userDetails: UserDetails
-) => {
-  const query='INSERT INTO user_department_mapping SET ? '
-  return runQuery(query,userDetails)
-}
-
 
 export const getDepartmentIdFromMapping = (userId: number) => {
-  const query='SELECT map_department_id FROM user_department_mapping WHERE map_user_id=? AND map_status="1"'
-  return runQuery(query,userId)
+  const query =
+    'SELECT map_department_id FROM user_department_mapping WHERE map_user_id=? AND map_status="1"'
+  return runQuery(query, userId)
 }
 
-export const changeMapStatusInDepartment = (userId:number)=>{
-  const query = 'UPDATE user_department_mapping SET map_status="0" WHERE map_user_id=?'
-  return runQuery(query,userId)
+export const changeMapStatusInDepartment = (userId: number) => {
+  const query =
+    'UPDATE user_department_mapping SET map_status="0" WHERE map_user_id=?'
+  return runQuery(query, userId)
 }
