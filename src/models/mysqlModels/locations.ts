4521183d290f732ca9800351a15c09d5346432e6
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 13:36:49 +0530

    createLocation query created in locations.ts

--- src/models/mysqlModels/locations.ts
+++ src/models/mysqlModels/locations.ts
@@ -1,6 +1,11 @@
 import { runQuery } from '../../databases/mysql'
 
 
+export const createLocation = (location: string) => {
+  const query='INSERT INTO locations (location_name,location_status) VALUES (?,"1")'
+  return runQuery(query,location)
+}
+
 export const getLocationId = (location: string) => {
   const query='SELECT location_id FROM locations WHERE location_name=?'
   return runQuery(query,location)
