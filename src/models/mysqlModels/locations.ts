f661df28b1f8f3df8f5e6077999e4df6c9e884ae
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 11:24:31 +0530

    query upadted in locations.ts

--- src/models/mysqlModels/locations.ts
+++ src/models/mysqlModels/locations.ts
@@ -15,11 +15,11 @@ const runQuery = (query: string, input: any) => {
 }
 
 export const getLocationId = (location: string) => {
-  const query='SELECT location_id from locations where location_name=?'
+  const query='SELECT location_id FROM locations WHERE location_name=?'
   return runQuery(query,location)
 }
 
 export const getLocationName = (locationId : number)=>{
-  const query='SELECT location_name from locations where location_id=?'
+  const query='SELECT location_name FROM locations WHERE location_id=?'
   return runQuery(query,locationId)
 }
