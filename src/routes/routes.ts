71377ab76974db7db9ca9117d347ab0a03a0d6f3
BuvanrajV <buvanraj.22.1@mountblue.tech>
2023-03-30 17:28:13 +0530

    "/token" endpoint added in routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,137 +1,141 @@
-import { Router } from 'express'
-import { body } from 'express-validator'
+import { Router } from "express";
+import { body } from "express-validator";
 import {
   postController,
   putController,
   deleteController,
-} from '../controllers/controller'
+  tokenController,
+} from "../controllers/controller";
 import {
   postMiddleware,
   putMiddleware,
   deleteMiddleware,
-} from '../middlewares/middlewares'
+  tokenMiddleware,
+} from "../middlewares/middlewares";
 
-const router = Router()
+const router = Router();
+
+router.get("/token", tokenMiddleware, tokenController);
 
 router.post(
   // Endpoint
-  '/post',
+  "/post",
   // Initial data validation using express validator
   [
-    body('candidateId')
+    body("candidateId")
       .isNumeric()
-      .withMessage('candidateId should be contain numbers only')
+      .withMessage("candidateId should be contain numbers only")
       .exists()
       .isLength({ max: 10 })
       .notEmpty()
-      .withMessage('CandidateId Should not be empty'),
-    body('firstName')
+      .withMessage("CandidateId Should not be empty"),
+    body("firstName")
       .isString()
       .exists()
       .isLength({ max: 150 })
       .isAlphanumeric()
-      .withMessage('Firstname should be in alphanumeric'),
-    body('lastName')
+      .withMessage("Firstname should be in alphanumeric"),
+    body("lastName")
       .isString()
       .isLength({ max: 150 })
       .isAlphanumeric()
-      .withMessage('Lastname Should be in alphanumeric'),
-    body('email')
+      .withMessage("Lastname Should be in alphanumeric"),
+    body("email")
       .isString()
       .exists()
       .isEmail()
-      .withMessage('email should be in email format'),
-    body('startDate')
+      .withMessage("email should be in email format"),
+    body("startDate")
       .isString()
       .exists()
       .isDate()
-      .withMessage('Date should be in YYYY-MM-DD this format')
+      .withMessage("Date should be in YYYY-MM-DD this format")
       .custom((startDate) => {
         // Check the startDate is greater than or equal to current date
-        let currentDateAndTime = new Date()
-        let currentDate = currentDateAndTime.toISOString().slice(0, 10)
+        let currentDateAndTime = new Date();
+        let currentDate = currentDateAndTime.toISOString().slice(0, 10);
         if (startDate < currentDate) {
           throw new Error(
-            'startDate should be greater than or equal to current date'
-          )
+            "startDate should be greater than or equal to current date"
+          );
         } else {
-          return 'no Error'
+          return "no Error";
         }
       }),
-    body('location')
+    body("location")
       .isLength({ max: 100 })
       .exists()
-      .withMessage('location field is missing')
+      .withMessage("location field is missing")
       .notEmpty()
-      .withMessage('Location should not be empty'),
-    body('department')
+      .withMessage("Location should not be empty"),
+    body("department")
       .isLength({ max: 100 })
       .exists()
-      .withMessage('department field is missing')
+      .withMessage("department field is missing")
       .notEmpty()
-      .withMessage('Department should not be empty'),
-    body('designation').isLength({ max: 100 }),
+      .withMessage("Department should not be empty"),
+    body("designation").isLength({ max: 100 }),
   ],
   // Middleware
   postMiddleware,
   // Controller
   postController
-)
+);
 
 router.put(
-  '/put', // Initial data validation using express validator
+  "/put", // Initial data validation using express validator
   [
-    body('candidateId')
+    body("candidateId")
       .isNumeric()
-      .withMessage('candidateId should be contain numbers only')
+      .withMessage("candidateId should be contain numbers only")
       .isLength({ max: 10 })
       .notEmpty()
-      .withMessage('CandidateId Should not be empty'),
-    body('firstName')
+      .withMessage("CandidateId Should not be empty"),
+    body("firstName")
       .isString()
       .isLength({ max: 150 })
       .isAlphanumeric()
-      .withMessage('Firstname should be in alphanumeric'),
-    body('lastName')
+      .withMessage("Firstname should be in alphanumeric"),
+    body("lastName")
       .isString()
       .isLength({ max: 150 })
       .isAlphanumeric()
-      .withMessage('Lastname Should be in alphanumeric'),
-    body('email')
+      .withMessage("Lastname Should be in alphanumeric"),
+    body("email")
       .isString()
       .isEmail()
-      .withMessage('email should be in email format'),
-    body('startDate')
+      .withMessage("email should be in email format"),
+    body("startDate")
       .isString()
       .isDate()
-      .withMessage('Date should be in YYYY-MM-DD this format')
+      .withMessage("Date should be in YYYY-MM-DD this format")
       .custom((startDate) => {
         // Check the startDate is greater than or equal to current date
-        let currentDateAndTime = new Date()
-        let currentDate = currentDateAndTime.toISOString().slice(0, 10)
+        let currentDateAndTime = new Date();
+        let currentDate = currentDateAndTime.toISOString().slice(0, 10);
         if (startDate < currentDate) {
           throw new Error(
-            'startDate should be greater than or equal to current date'
-          )
+            "startDate should be greater than or equal to current date"
+          );
         } else {
-          return 'no Error'
+          return "no Error";
         }
       }),
-    body('location')
+    body("location")
       .isLength({ max: 100 })
-      .withMessage('location field is missing')
+      .withMessage("location field is missing")
       .notEmpty()
-      .withMessage('Location should not be empty'),
-    body('department')
+      .withMessage("Location should not be empty"),
+    body("department")
       .isLength({ max: 100 })
-      .withMessage('department field is missing')
+      .withMessage("department field is missing")
       .notEmpty()
-      .withMessage('Department should not be empty'),
-    body('designation').isLength({ max: 100 }),
+      .withMessage("Department should not be empty"),
+    body("designation").isLength({ max: 100 }),
   ],
   putMiddleware,
   putController
-)
-router.delete('/delete', deleteMiddleware, deleteController)
+);
+router.delete("/delete", deleteMiddleware, deleteController);
 
-export default router
+export default router;
